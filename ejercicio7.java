5d908a5c4545b08871d748320e70c4b5fcb93b5b
jesusvalla915-lgtm <jesusvalla915@mi.unc.edu.ar>
2026-02-05 18:05:55 -0300

    Create ejercicio7.java
    Resuelto

---
+++ ejercicio7.java
@@ -0,0 +1,20 @@
+//Ejercicio 7 del final de programacion avanzada 
+public class Karting extends Vehiculo {
+    
+    public Karting(){
+        super();
+    }
+    public void to
+}
+
+//Ejercicio Resuelto
+
+public class Karting extends Vehiculo {
+    
+    public Karting(String propietario){
+        super(4,propietario);
+    }
+    public String toString(){
+        return "Karting de " + get.propietario() + "(" + super.toString() ")";
+    }
+}
